3448c540b6f79a90ff06d7ea71235baf4535063b
Christine Hümmer <christine.huemmer.fs@gmail.com>
2020-03-23 11:28:25 +0100

    Update#1 23/03/2020 Update Navbar

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -1,15 +1,4 @@
-$(document).ready(function(){
-    
-  "use strict"; var scrollTop = 0;
-
-    $(window).scroll(function(){
-      scrollTop = $(window).scrollTop();
-       $('.counter').html(scrollTop);
-      
-      if (scrollTop >= 100) {
-        $('.global-nav').addClass('-collapse');
-      } else if (scrollTop < 100) {
-        $('.global-nav').removeClass('-collapse');
-      }    
-    });  
-  });
+//jQuery Smooth Scroll
+var scroll = new SmoothScroll('.navbarTogglerDemo02 a[href*="#"]', {
+  speed: 800,
+});
