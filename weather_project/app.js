ebdbc77f220ed1807d9525b484253c3fab0ff220
Christine Hümmer <christine.huemmer.fs@gmail.com>
2020-04-01 18:02:42 +0200

    upload app.js

--- weather_project/app.js
+++ weather_project/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/weather_project/weather.html");
+  res.sendFile(__dirname + "/weather.html");
 })
 
 app.post("/", function(req, res) {
